a06706f5dbb20725e0141d8b3b3f85990de950cc
nitin-codr <nitinporwal99@gmail.com>
2021-01-11 16:14:29 +0530

    add question page

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.quizapp
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        var startBtn = findViewById<Button>(R.id.btn_start)
+        var nameEt = findViewById<EditText>(R.id.et_name)
+        startBtn.setOnClickListener {
+            if(nameEt.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -2,10 +2,40 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        val questionList = Constants.getQuestions()
+        var currentPositon = 1
+        var question: Question? = questionList[currentPositon-1]
+
+        var progress = findViewById<ProgressBar>(R.id.progressBar)
+        progress.progress = currentPositon
+        val progressTv = findViewById<TextView>(R.id.tv_progress)
+        progressTv.text = "$currentPositon" + "/" + progress.max
+
+        var questionTv = findViewById<TextView>(R.id.tv_question)
+        questionTv.text = question!!.question
+
+        var imgIv = findViewById<ImageView>(R.id.iv_image)
+        imgIv.setImageResource(question.image)
+
+        var optionOneTv = findViewById<TextView>(R.id.tv_option_one)
+        optionOneTv.text = question.optionOne
+
+        var optionTwoTv = findViewById<TextView>(R.id.tv_option_two)
+        optionTwoTv.text = question.optionTwo
+
+        var optionThreeTv = findViewById<TextView>(R.id.tv_option_three)
+        optionThreeTv.text = question.optionThree
+
+        var optionFourTv = findViewById<TextView>(R.id.tv_option_four)
+        optionFourTv.text = question.optionFour
     }
 }
